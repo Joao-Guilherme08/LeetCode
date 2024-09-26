843ce565d55a0480b3ee8960c451167ef9a2e6c9
João Guilherme Dias <Alwaysstrong08@hotmail.com>
2024-09-26 18:11:21 -0300

    Create  RentalService.cs

---
+++ Nelio12_Interfaces/Services/RentalService.cs
@@ -0,0 +1,38 @@
+using PrimeiroProjeto.Entities;
+using System;
+
+namespace PrimeiroProjeto.Services
+   
+{
+    class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        private ITaxService _taxService; //INVERSÃO DE CONTROLE POR MEIO DE INJEÇÃO DE DEPENDENCIA
+        public RentalService(double pricePerHour, double pricePerDay, ITaxService taxService)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+            _taxService = taxService;
+        }
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+
+            double basicPayment = 0.0;
+            if (duration.TotalHours <= 12.00)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+            double tax = _taxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+            
+        }
+    }
+}
